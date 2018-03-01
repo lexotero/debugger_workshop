a609c22793aae47518417e9dfa14ec6a6e21e758
Alejandro Otero Ortiz de Cosca <otero.alx@gmail.com>
2018-03-01 13:42:55 +0000

    Setup different endpoints for static content and root.

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const app = express();
 
 const staticFilesDir = path.join(__dirname, 'app/public');
 
-app.use(express.static(staticFilesDir));
+app.get('/', function(req, res) {
+    res.send('Hello debugger!')
+});
 
-//app.get('/', function(req, res) {
-//    res.send('Hello World!')
-//});
+app.use('/static', express.static(staticFilesDir));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
